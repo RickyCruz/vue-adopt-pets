f1f05c292f06b28b9bea8929391304d0df3c53da
Ricky Cruz <rczdev@gmail.com>
2020-05-27 16:02:00 -0500

    Add mutations & actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,3 +1,9 @@
 export default {
-
+  // addPet: (context, pet) => {
+  //   context.commit('appendPet', pet)
+  // }
+  // Popular way to use
+  addPet: ({ commit }, playload) => {
+    commit('appendPet', playload)
+  }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,83 @@
 <template>
   <div class="home">
     <h1>Adopt a new best friend.</h1>
+    <button @click="togglePetForm" class="btn btn-primary">Add New Pet</button>
+
+    <b-form @submit.prevent="handleSubmit" v-if="showPetForm">
+      <b-form-group id="pet-name-group" label="Pet's Name:" label-for="pet-name">
+        <b-form-input
+          id="pet-name"
+          type="text"
+          v-model="formData.name"
+          required
+          placeholder="Enter name" />
+      </b-form-group>
+
+      <b-form-group id="pet-species-group" label="Species:" label-for="pet-species">
+        <b-form-select id="pet-species" :options="['cats', 'dogs']" required v-model="formData.species" />
+      </b-form-group>
+
+      <b-form-group id="pet-age-group" label="Pet's Age:" label-for="pet-age">
+        <b-form-input
+          id="pet-age"
+          type="number"
+          v-model="formData.age"
+          required
+          placeholder="Enter age" />
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+
+  data () {
+    return {
+      showPetForm: false,
+      formData: {
+        name: '',
+        age: 0,
+        species: null
+      }
+    }
+  },
+
+  methods: {
+    ...mapActions([
+      'addPet'
+    ]),
+
+    togglePetForm () {
+      this.showPetForm = !this.showPetForm
+    },
+
+    handleSubmit () {
+      const { species, age, name } = this.formData
+
+      const payload = {
+        species,
+        pet: {
+          name,
+          age
+        }
+      }
+
+      this.addPet(payload)
+
+      // Reset form after submit
+      this.formData = {
+        name: '',
+        age: 0,
+        species: null
+      }
+    }
+  }
 }
 </script>
